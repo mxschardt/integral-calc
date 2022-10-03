d9b5f98c5a60d01d83f28db0b6188b6dde5d3595
Maxim Schardt <mxschardt@gmail.com>
2022-10-03 17:53:01 +0300

    Initial Setup

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import reactLogo from '/react.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <h1>Hello World!</h1>
+
+      <div className="card">
+        <button
+          className="count-btn"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          count is {count}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
