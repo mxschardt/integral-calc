5afe971a997b0b8ca3c55b3f01902b68582346f8
Maxim Schardt <mxschardt@gmail.com>
2022-10-30 12:21:38 +0300

    routing added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Integral from './pages/Integral';
 import MultiplyIntegral from './pages/MultiplyIntegral';
@@ -5,8 +6,10 @@ import MultiplyIntegral from './pages/MultiplyIntegral';
 function App() {
   return (
     <Layout>
-      <Integral />
-      <MultiplyIntegral />
+      <Routes>
+        <Route path="/" element={<Integral />} />
+        <Route path="/multiple" element={<MultiplyIntegral />} />
+      </Routes>
     </Layout>
   );
 }
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,9 +1,20 @@
+import { Link } from 'react-router-dom';
 import './styles/layout.css';
 
 function Layout({ children }: { children?: React.ReactNode }) {
   return (
     <section className="layout">
       <h1>Калькулятор Определенных Интегралов</h1>
+      <nav className='navbar'>
+        <ul>
+          <li>
+            <Link to='/' className='nav-item'>Определенный Интеграл</Link>
+          </li>
+          <li>
+            <Link to='/multiple' className='nav-item'>Кратный Интеграл</Link>
+          </li>
+        </ul>
+      </nav>
 
       {children}
 
